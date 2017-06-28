bf9cced7e29ed44b729012f38e15c2b4060cded2
Trevor Joynson (trevorj) <github@skywww.net>
2017-06-28 16:41:08 -0700

    Fix test comparison

--- tests/test_dispatch.py
+++ tests/test_dispatch.py
@@ -1,16 +1,31 @@
 import uninhibited
-import six
+import itertools
+import random
 
-EVENT_NAMES = ['one', 'two', 'three']
+from faker import Factory
+fake = Factory.create()
+
+
+def iter_event_names():
+    while True:
+        name = fake.name()
+        yield name
+        yield name.replace(' ', '_')
 
 
 def test_add_events():
+    event_names = list(itertools.islice(iter_event_names(), random.randint(10, 20)))
     d = uninhibited.Dispatch()
-    d.add_events(EVENT_NAMES)
-    assert list(six.keys(d.events)) == EVENT_NAMES
+
+    d.add_events(event_names)
+
+    ret = set(event_names) - set(d.events)
+    return not ret
 
 
 def test_given_event_names_are_added():
-    d = uninhibited.Dispatch(EVENT_NAMES)
-    assert list(six.keys(d.events)) == EVENT_NAMES
+    event_names = set(itertools.islice(iter_event_names(), random.randint(10, 20)))
+    d = uninhibited.Dispatch(event_names)
 
+    ret = set(event_names) - set(d.events)
+    return not ret
